322867d3a59ffac48806f8026a9c5f9da7eee2a8
SahilGunjal <gunjal.zswyv21@sinhgad.edu>
2023-10-22 09:29:20 +0530

    Nested Updated

---
+++ Nested_for.py
@@ -0,0 +1,21 @@
+for i in range(2):
+    print("In i for Loop")
+    for j in range(3):
+        print("In j for loop")
+print("Out of For")
+
+for i in range(3):
+    for j in range(3):
+        print("*",end=" ")
+
+print()
+
+
+
+for i in range(3):
+    num = 1
+    for j in range(3):
+        print(num, end=" ")
+        num=num+1
+    print()
+
