aba5e3f168df9bcc111b4b7d573d151366dd099c
SahilGunjal <gunjal.zswyv21@sinhgad.edu>
2023-10-29 20:02:45 +0530

    Nested all programs

--- Nested_for.py
+++ Nested_for.py
@@ -1,16 +1,26 @@
+'''
 for i in range(2):
     print("In i for Loop")
     for j in range(3):
         print("In j for loop")
 print("Out of For")
+'''
+
+'''
+**********
 
 for i in range(3):
     for j in range(3):
         print("*",end=" ")
 
 print()
+'''
 
+'''
 
+    1 2 3
+    1 2 3
+    1 2 3
 
 for i in range(3):
     num = 1
@@ -18,4 +28,85 @@ for i in range(3):
         print(num, end=" ")
         num=num+1
     print()
+'''
+
+'''
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+for i in range(3):
+    for j in range(3):
+        print("*",end=" ")
+        print()
+'''
+
+'''
+
+    * * *
+    * * *
+    * * *
+    
+    for i in range(3):
+    for j in range(3):
+        print("*",end=" ")
+    print()
+'''
+
+
+'''
+    1 2 3
+    4 5 6
+    7 8 9
+    
+    num=1
+for i in range(3):
+    for j in range(3):
+        print(num,end=" ")
+        num+=1
+    print()
+
+'''
+
+
+'''
+    1 1 1
+    2 2 2 
+    3 3 3
+    
+    num=1
+for i in range(3):
+    for j in range(3):
+        print(num,end=" ")
+    print()
+    num+=1
+'''
+
+
+'''
+    1 2 3 
+    1 2 3
+    1 2 3
+for i in range(3):
+    num=1
+    for j in range(3):
+        print(num,end=" ")
+        num+=1
+    print()
+'''
+
+
+
+
+
+
+
+
+
+
 
