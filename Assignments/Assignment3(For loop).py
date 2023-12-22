d96c6c3d4119d4779287753dd034fc280c1e568b
SahilGunjal <gunjal.zswyv21@sinhgad.edu>
2023-12-22 14:04:10 +0530

    Assignment 3 Added

---
+++ Assignments/Assignment3(For loop).py
@@ -0,0 +1,57 @@
+# 1.Write a program to print the first 10 numbers
+
+# for i in range(1,11):
+#     print(i)
+
+# 2.Write a program to print the first 100 numbers
+
+# for i in range(1,101):
+#     print(i)
+
+# 3.Write a program to print the first ten, 3 digit number
+
+# for num in range(100, 1000):
+#     print(num)
+#     if num == 109:
+#         break
+#
+
+# 4.Write a program to print even numbers 1-100
+#
+# for i in range(0,101):
+#     if(i%2==0):
+#         print(i)
+
+# 5.Write a program to print odd numbers 1-50
+
+# for i in range(0,51):
+#     if i%2!=0:
+#         print(i)
+
+# 6.Write a program to print reverse from 100-1
+#
+# for i in range(100,0,-1):
+#     print(i)
+
+#7.Write a program to print a table of 12
+
+# for i in range(0,121,12):
+#     print(i)
+
+#8.Write a program to print a table of 12 in reverse order.
+# for i in range(12, 0, -1):
+#     result = 12 * i
+#     print(f"12 x {i} = {result}")
+
+#9.Write a program to print the sum of the first 10 numbers
+# sum=0
+# for i in range(0,11):
+#     sum=sum+i
+# print(sum)
+
+#10.Write a program to print the product of the first 10 numbers
+
+mul=1
+for i in range(1,11):
+    mul=mul*i
+print(mul)
