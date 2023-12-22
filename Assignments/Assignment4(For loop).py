59b4f086d3656d4ca31e6069a31b4767d23a9e3b
SahilGunjal <gunjal.zswyv21@sinhgad.edu>
2023-12-22 15:04:43 +0530

    Assignment 4 Added

---
+++ Assignments/Assignment4(For loop).py
@@ -0,0 +1,53 @@
+#1.WAP to print numbers from a given range. Start = 100; End = 110;
+# for i in range(100,110):
+#     print(i)
+
+#2.Write a Program to print all Even numbers from a given range.Start = 10;End = 20;
+
+# for i in range(10,20,2):
+#     print(i);
+
+# #3.WAP to print the sum of all numbers from a given range. Start:1,End: 10
+# sum=0
+# for i in range(1,10):
+#     sum=sum+i;
+# print(sum)
+
+#4.WAP to print all the character values of the given ASCII value range from the user.
+# for i in range(65,90):
+#     print("Character of ASCII value {} is {}".format(i,chr(i)))
+
+#5.WAP to print the number divisible by 7 but not divisible by 3 between 1 to 100.Enter start of range - 1 Enter end of range - 100
+#
+# for i in range(1,100):
+#     if i%7==0 and i%3!=0:
+#         print(i)
+
+#6.WAP to print all the ASCII values from a given character range.Enter the start of range : A Enter end of range: Z
+
+# for i in range(ord('A'),ord('Z')):
+#     print("ASCII value of {} is {}".format(chr(i),i))
+
+#7.WAP that prints all Positive numbers from a given range.
+#
+# for i in range(-7,8):
+#     if i>0:
+#         print(i)
+
+#8.WAP to prints numbers which are divisible by 3 and 5 both in a given range.
+# for i in range(15,50):
+#     if i%3==0 and i%5==0:
+#         print(i)
+
+#9.WAP to print the count of all negative numbers from a given range
+
+# count=0
+# for i in range(-15,50):
+#     if i < 0:
+#         count+=1
+#
+# print(count)
+
+
+
+
